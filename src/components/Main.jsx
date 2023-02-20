1c1e3f080bb979a39a0c54017633cec8277ff62f
Persona743 <siyavashshahidi7@gmail.com>
2023-02-20 20:48:39 +0330

    main-button added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,12 +7,14 @@ export default function Main() {
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created bt Jordan Walke</li>
-                <li>Has wll over 100k stars on Github</li>
+                <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>
                     powers thousands of enterprise apps, including mobile apps
                 </li>
             </ul>
+
+            <a href="">Go to card section</a>
         </main>
     );
 }
