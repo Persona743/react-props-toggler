f59c40a210fd76b52814d0fa5da396bdfeb0b81c
Persona743 <siyavashshahidi7@gmail.com>
2023-02-20 18:11:47 +0330

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created bt Jordan Walke</li>
+                <li>Has wll over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>
+                    powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ul>
+        </main>
+    );
+}
