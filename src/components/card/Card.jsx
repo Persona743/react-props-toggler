d79799b8cac894fdb4a892e701cf183048706122
Persona743 <siyavashshahidi7@gmail.com>
2023-02-21 20:30:17 +0330

    button added

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Info from './Info';
 import AboutInterest from './AboutInterest';
 import Footer from './Footer';
@@ -9,6 +10,7 @@ export default function Card() {
             <Info />
             <AboutInterest />
             <Footer />
+            <Link to="/">Go Back </Link>
         </div>
     );
 }
