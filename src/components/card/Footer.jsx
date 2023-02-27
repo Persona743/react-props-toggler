903d3dd311b734e98eb8b161a388d49f06f82fac
Persona743 <siyavashshahidi7@gmail.com>
2023-02-27 20:04:19 +0330

    footer fixed

--- src/components/card/Footer.jsx
+++ src/components/card/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className="footer--wrap">
-            <img src="/assets/images/twitterIcon" className="social--icon" />
+            <img
+                src="/assets/images/Twitter Icon.png"
+                className="social--icon"
+            />
             <img
                 src="/assets/images/Facebook Icon.png"
                 className="social--icon"
@@ -12,7 +15,10 @@ export default function Footer() {
                 src="/assets/images/Instagram Icon.png"
                 className="social--icon"
             />
-            <img src="/assets/images/githubIcon" className="social--icon" />
+            <img
+                src="/assets/images/GitHub Icon.png"
+                className="social--icon"
+            />
         </div>
     );
 }
